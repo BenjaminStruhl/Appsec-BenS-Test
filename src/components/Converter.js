4e7b4b3af80afecf0525f8fcf5528b29a34d0455
BenjaminStruhl <benjamin.struhl@tempus.com>
2021-05-27 12:56:32 -0500

    last changes

---
+++ src/components/Converter.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+let convert = (input) => {
+  let output = ""
+  for (let i = 0; i < input.length; i++) {
+      output += input[i].charCodeAt(0).toString(2) + " ";
+  }
+  return output
+}
+
+function Converter() {
+  const [converted_text, setConvertedText] = useState("Your converted text will appear here");
+    return (
+      <div>
+        <h1>Evil Text to Binary Converter</h1>
+        <div className="container_row">
+        <div className="layer1" dangerouslySetInnerHTML={ {__html: converted_text}}></div>
+        <textarea className="layer2" onChange={(e) => setConvertedText(e.target.value)}></textarea>
+        <button className="layer3" onClick={() => {setConvertedText(convert(converted_text))}}>convert</button>
+      </div>
+      </div>
+    );
+  }
+  
+  export default Converter;
+  
